57640ccbcf05edd802129cbcb668e3ab66ede58e
TeoLiss <fedya199797@gmail.com>
2023-06-15 11:40:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+
+        static void SplitText(string text)
+        {
+            //text = Console.ReadLine();
+            string[] splited = text.Split(' ');
+            foreach (var i in splited)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string text = Console.ReadLine();
+
+            SplitText(text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task5-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string SplitWord(string text) // 
+            {
+                string[] wordsSplit = text.Split(' ');
+                string newText = ReverseWords(wordsSplit);
+                return newText;
+            }
+            string ReverseWords(string[] text) // 
+            {
+                Array.Reverse(text);
+                string str = string.Join(" ", text);
+                return str;
+            }
+            
+            Console.WriteLine("Задание 2. Перестановка слов в предложении");
+            Console.WriteLine("Введите предложение: ");
+
+            string test2 = Console.ReadLine();
+
+            test2 = SplitWord(test2);
+
+
+            Console.WriteLine(test2);
+            Console.ReadLine();
+        }
+    }
+}
